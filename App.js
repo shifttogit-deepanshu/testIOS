897a093667d66efd7a7d8d264b4e0e3362578964
Deepanshu <sharmadeepanshu.kaizen1@gmail.com>
2022-10-11 04:27:07 +0530

    init commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>You have built this app for {Platform.OS}</Text>
       <StatusBar style="auto" />
     </View>
   );
